d3f62f94f025cecabbcea759dbdf847a248e27bc
moondiverc <nezzaluna10@gmail.com>
2024-11-21 11:24:41 +0700

    Created navbar and hero

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,5 +1,11 @@
 function Navbar() {
-  return <>{/* TODO */}</>;
+  return <nav className="navbar">
+  <a href="#" className="navbar-logo">GoExplore Indonesia</a>
+  <div className="navbar-nav">
+  <a href="#destination">Destinations</a>
+  <a href="#book">Book a Tour</a>
+  </div>
+</nav>;
 }
 
 export default Navbar;
